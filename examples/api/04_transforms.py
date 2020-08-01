2c05c4d0155700c240284493bcff1e2bff127d49
Michael DeHaan <michael@michaeldehaan.net>
2020-08-01 08:04:29 -0400

    a few more comments in some of the examples

--- examples/api/04_transforms.py
+++ examples/api/04_transforms.py
@@ -5,7 +5,10 @@
 #
 # this demo shows how transforms work (see docs!) and how
 # to build a simple arpeggiator, as well as other MIDI effects.
-
+#
+# warning: as with all demos, whether these demos sound
+# musical may vary based on your chosen instruments! These are mostly
+# to illustrate concepts, and the compositions are up to you.
 
 from warpseq.api import demo
 from warpseq.api.public import Api as WarpApi
@@ -40,12 +43,19 @@ api.scenes.add(name='scene_6', rate=0.5, auto_advance=True)
 api.scenes.add(name='scene_7', rate=0.5, auto_advance=True)
 
 # setup transforms
+# arpeggiate a triad
 api.transforms.add(name='basic arp', slots=['1'], divide=3)
+# play two copies of the chord, the second one octave up
 api.transforms.add(name='octave arp', slots=['1','1','1','O+1','O+1','O+1'], divide=6)
+# play each note in a triad with diminished velocity (this might not be audible, depending on your synth settings)
 api.transforms.add(name='velocity arp', slots=['1 v=120','1 v=100','1 v=80'], divide=3)
+# play each note in a triad with different MIDI CC values for MIDI CC 1
 api.transforms.add(name='midi cc arp', slots=['1 cc1=80', '1 cc1=100', '1 cc1=20:100'], divide=3)
+# take the base note of a pattern and then play it faster, shifting the scale notes to form a bassline
 api.transforms.add(name='bassline', slots=['1','S+4','S+5','S+2','S+4','S+5','1'], divide=3)
+# play the second note of a triad or pattern one note up, the second two notes up
 api.transforms.add(name='octave ramp', slots=['1','O+1','O+2'], divide=1)
+# quickly repeat the notes with alternating silence, the last repeat is only randomly silent
 api.transforms.add(name='stutter', slots=['1','x','1','x','1','p=0.5 x'], divide=6)
 
 # setup clips
@@ -54,6 +64,8 @@ api.clips.add(name='chord octaves', scene='scene_2', track='lead', scales=['C-ma
 api.clips.add(name='chord velocity', scene='scene_3', track='lead', scales=['C-major'], patterns=['chords'], transforms=['velocity arp'], repeat=1, auto_scene_advance=True)
 api.clips.add(name='chord ccs', scene='scene_4', track='lead', scales=['C-major'], patterns=['chords'], transforms=['midi cc arp'], repeat=1, auto_scene_advance=True)
 api.clips.add(name='melody to bassline', scene='scene_5', track='lead', scales=['C-major'], patterns=['basic'], transforms=['bassline'], repeat=1, auto_scene_advance=True)
+
+# the transforms can be expressed in a list, the next item in the transform list will be chosen as the patterns advance and repeat
 api.clips.add(name='melody octave adjustment, then stutter', scene='scene_6', track='lead', scales=['C-major'], patterns=['basic'], transforms=['octave ramp', 'stutter'], repeat=2, auto_scene_advance=True)
 api.clips.add(name='stacked transforms', scene='scene_7', track='lead', scales=['C-major'], patterns=['basic'], transforms=[['octave ramp','stutter'],'bassline',['octave arp','basic arp']], repeat=3,  auto_scene_advance=True)
 
--- examples/api/05_clip_features.py
+++ examples/api/05_clip_features.py
@@ -12,7 +12,7 @@ from warpseq.api.public import Api as WarpApi
 
 # setup API and song
 api = WarpApi()
-api.song.edit(tempo=160)
+api.song.edit(tempo=120)
 
 # setup instruments
 DEVICE = demo.suggest_device(api, 'IAC Driver IAC Bus 1')
@@ -35,23 +35,44 @@ api.scenes.add(name='scene_1', rate=1, auto_advance=False)
 api.scenes.add(name='scene_2', rate=1, auto_advance=False)
 
 # setup transforms
-api.transforms.add(name='stutter', slots=['1', 'x', '1', '1', '1', 'x'], divide=3)
-api.transforms.add(name='chance octave jump', slots=['1','1','1','O=0,0,0,1,2'], divide=3)
-api.transforms.add(name='octave up', slots=['O+1'], divide=1)
-api.transforms.add(name='octave hop', slots=['1', 'O+1'], divide=1)
-api.transforms.add(name='every third note up 5th', slots=['1','1','S+5'], divide=1)
 
+# play multiple copies of notes + insert some silence patterns into notes
+api.transforms.add(name='stutter', slots=['1', 'x', '1', '1', '1', 'x'], divide=3)
+
+# play multiple copies of notes + every fourth repeat has a 2 in 5 chance of jumping up 1 or 2 octaves
+api.transforms.add(name='chance octave jump', slots=['1','1','1','O=0,0,0,1,2'], divide=3)
+
+# notes play at the same speed - every note jumps up an octave
+api.transforms.add(name='octave up', slots=['O+1'], divide=1)
+
+# notes play at the same speed - every other note jumps up an octave
+api.transforms.add(name='octave hop', slots=['1', 'O+1'], divide=1)
+
+# notes play at the same speed - every third note jumps up a fifth vs what was in the original pattern
+api.transforms.add(name='every third note up 5th', slots=['1','1','S+5'], divide=1)
 
 # setup clips
 api.clips.add(name='kitchen sink',
               scene='scene_1',
               track='lead',
+              # each time we play a new pattern we will walk through using a different scale
+              # after the third pattern this loops back around
               scales=['G-phyrigian', 'F-mixolydian', 'magic'],
               patterns=['up', 'down'],
-              transforms=[['chance octave jump'],['octave up', 'stutter'],['octave hop', 'every third note up 5th']],
+              # each time we play a new pattern we will walk through and choose a new set of transforms
+              # from this list. After the FOURTH pattern this will loop around
+              transforms=[['octave up'],['octave up', 'stutter'],['octave hop', 'every third note up 5th'], ['chance octave jump']],
+              # each pattern goes a little faster than the previous (for a while, anyway, then it cycles)
               tempo_shifts = [0, 5, 10, 15, 20, 25, 30 ],
-              repeat=20,
+              # we'll play this same clip for a long time. Each repeat includes playing all of the patterns
+              # so this means 8 total patterns will play
+              repeat=4,
+              # previous examples have shown the 'auto scene advance' feature, here we also show that we can jump
+              # to specific clips in the same track if we don't use the scene advance
               next_clip='ending')
+
+
+# this is just a boring clip without using many clip features, but it does show the rate
 
 api.clips.add(name='ending',
               scene='scene_2',
@@ -59,6 +80,7 @@ api.clips.add(name='ending',
               scales=['magic'],
               patterns=['down'],
               repeat=2,
+              # this is part of our finale, so play it slower
               rate=0.25)
 
 # play starting on the first scene - Ctrl+C to exit.
