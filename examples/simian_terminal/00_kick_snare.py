d9cf8ce4e9f9ddbf0c7bbdc86717ae20b3895bd7
Jason S Fillman <github@jasonfillman.com>
2020-08-10 23:00:42 -0700

    Adding branch and dir for experimentation

---
+++ examples/simian_terminal/00_kick_snare.py
@@ -0,0 +1,53 @@
+# -------------------------------------------------------------
+# Warp API Demo
+# (C) Michael DeHaan <michael@michaeldehaan.net>, 2020
+# -------------------------------------------------------------
+#
+# this demo shows a basic 4/4 kick/snare pattern using absolute
+# notes. It will not be adjusted by a scale assignment, which is
+# important when talking to drum tracks.
+#
+# after the first basic pattern plays, the pattern changes up,
+# showing how to advance a clip into a second scene.
+#
+# try changing the patterns and adding a third scene.
+
+from warpseq.api import demo
+from warpseq.api.callbacks import Callbacks, DefaultCallback
+from warpseq.api.public import Api as WarpApi
+
+# setup API and song
+api = WarpApi()
+api.song.edit(tempo=120)
+
+# setup instruments
+DEVICE = demo.suggest_device(api, 'IAC Driver IAC Bus 1')
+api.instruments.add('kick_inst', device=DEVICE, channel=1, min_octave=0, base_octave=0, max_octave=10)
+api.instruments.add('snare_inst', device=DEVICE, channel=2, min_octave=0, base_octave=0, max_octave=10)
+
+# setup tracks
+api.tracks.add(name='kick', instrument='kick_inst', muted=False)
+api.tracks.add(name='snare', instrument='snare_inst', muted=False)
+
+# setup scales
+# api.scales.add(name='C-major', note='C', octave=3, scale_type='major')
+
+# setup patterns
+api.patterns.add(name='kick_4_4',  slots="C1 . . . C1 . . . C1 . . . C1 . . .".split())
+api.patterns.add(name='snare_4_4', slots=". . D1 . . . D1 . . . D1 . . . D1 .".split())
+api.patterns.add(name='kick_alt',  slots="C1 . C1 . C1 . .  . C1 . .  . C1 . .  .".split())
+api.patterns.add(name='snare_alt', slots=".  . D1 . .  . D1 D1 .  D1 D1 . .  D1 D1 .".split())
+
+# setup scenes
+api.scenes.add(name='scene_1', scale=None, auto_advance=True)
+#api.scenes.add(name='scene_2', scale='C-major', auto_advance=True)
+
+# setup clips
+api.clips.add(name='s1k1', scene='scene_1', track='kick', patterns=['kick_4_4'], repeat=100, auto_scene_advance=True)
+api.clips.add(name='s1s1', scene='scene_1', track='snare', patterns=['snare_4_4'], repeat=100)
+# change the drum pattern, repeat=None implies infinite repeat.
+#api.clips.add(name='s2k1', scene='scene_2', track='kick', patterns=['kick_alt'], repeat=4, auto_scene_advance=True)
+#api.clips.add(name='s2s1', scene='scene_2', track='snare', patterns=['snare_alt'], repeat=4)
+
+# play starting on the first scene - Ctrl+C to exit.
+api.player.loop('scene_1')
---
+++ examples/simian_terminal/01_scales.py
@@ -0,0 +1,68 @@
+# --------------------------------------------------------------
+# Warp API Demo
+# (C) Michael DeHaan <michael@michaeldehaan.net>, 2020
+# --------------------------------------------------------------
+#
+# this demo shows how to enter scale notes as degrees that
+# change automatically as the scales change and always
+# sound musically relevant
+#
+# scales can be set on the song and overridden per scene
+# (for easy mid-song key changes) or even per clip - where they
+# can be expressed in a list. If a list is passed to a clip, the scales
+# will rotate as each pattern in the clip advances.
+#
+# try changing the patterns and scales
+
+from warpseq.api import demo
+from warpseq.api.public import Api as WarpApi
+
+# setup API and song
+api = WarpApi()
+api.song.edit(tempo=120)
+
+# setup instruments
+DEVICE = demo.suggest_device(api, 'IAC Driver IAC Bus 1')
+api.instruments.add('lead_inst', device=DEVICE, channel=1, min_octave=0, base_octave=5, max_octave=10)
+
+# setup tracks
+api.tracks.add(name='lead', instrument='lead_inst', muted=False)
+
+# setup scales
+print("available scale types:")
+print(api.scales.scale_types())
+api.scales.add(name='C-major', note='C', octave=0, scale_type='major')
+api.scales.add(name='C-minor', note='C', octave=0, scale_type='natural_minor')
+api.scales.add(name='G-major', note='G', octave=0, scale_type='major')
+api.scales.add(name='G-minor', note='G', octave=0, scale_type='natural_minor')
+api.scales.add(name='Bb-mixolydian', note='Bb', octave=0, scale_type='mixolydian')
+api.scales.add(name='A-akebono', note='A', octave=0, scale_type='akebono')
+api.scales.add(name='F-user1', note='F', octave=0, slots = [1, 'b2', 'b3', '5', 6 ])
+api.scales.add(name='F-user2', note='F', octave=1, slots = [1, 'b2', 'b3', '5', 6 ])
+
+# setup patterns
+api.patterns.add(name='up', slots=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'])
+api.patterns.add(name='down', slots=['8','7','6','5','4','3','2','1'])
+
+# setup scenes
+api.scenes.add(name='scene_1', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_2', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_3', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_4', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_5', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_6', rate=0.5, auto_advance=True)
+api.scenes.add(name='scene_7', rate=1, auto_advance=True)
+api.scenes.add(name='scene_8', rate=1, auto_advance=True)
+
+# setup clips
+api.clips.add(name='s1c', scene='scene_1', track='lead', scales=['C-major'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s2c', scene='scene_2', track='lead', scales=['C-minor'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s3c', scene='scene_3', track='lead', scales=['G-major'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s4c', scene='scene_4', track='lead', scales=['G-minor'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s5c', scene='scene_5', track='lead', scales=['Bb-mixolydian'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s6c', scene='scene_6', track='lead', scales=['A-akebono'], patterns=['up'], repeat=1, auto_scene_advance=True)
+api.clips.add(name='s7c', scene='scene_7', track='lead', scales=['C-major','G-minor','A-akebono'], patterns=['down'], repeat=3, auto_scene_advance=True)
+api.clips.add(name='s8c', scene='scene_8', track='lead', scales=['F-user1', 'F-user2'], patterns=['down'], repeat=2, auto_scene_advance=True)
+
+# play starting on the first scene - Ctrl+C to exit.
+api.player.loop('scene_1')
